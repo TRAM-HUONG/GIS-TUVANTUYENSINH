ff63c97744c5822f6e4c35381e7a146e969e5d11
TRAM-HUONG <1250080066@sv.hcmunre.edu.vn>
2026-03-09 10:40:26 +0700

    Lưu các thay đổi

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
 
 urlpatterns = [
     path("", views.home_page, name="home"),
     path("truong/", views.truong_list, name="truong_list"), 
     path("truong/<str:matruong>/", views.truong_detail, name="truong_detail"),
     path("nganh/<str:manganh>/", views.nganh_detail, name="nganh_detail"),
+    path("admin/", admin.site.urls),
 ]
