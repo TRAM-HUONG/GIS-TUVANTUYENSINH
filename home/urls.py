728b99302f805f5ae5fb92b7cdfe39337a07d7f0
TRAM-HUONG <1250080066@sv.hcmunre.edu.vn>
2026-03-12 03:05:19 +0700

    Lưu các thay đổi

--- home/urls.py
+++ home/urls.py
@@ -8,6 +8,10 @@ urlpatterns = [
     path("truong/<str:matruong>/", views.truong_detail, name="truong_detail"),
     path("nganh/<str:manganh>/", views.nganh_detail, name="nganh_detail"),
     path("admin/", admin.site.urls),
-    path("", map_view, name="map"),
+    path('admin/truong-dai-hoc/', views.admin_truong_list, name='admin_truong_list'),
+    path('admin/truong-dai-hoc/them/', views.admin_truong_insert, name='admin_truong_insert'),
+    path('admin/truong-dai-hoc/chi-tiet/<str:matruong>/', views.admin_truong_detail, name='admin_truong_detail'),
+    path('admin/truong-dai-hoc/sua/<str:matruong>/', views.admin_truong_edit, name='admin_truong_edit'),
+    path('admin/truong-dai-hoc/xoa/<str:matruong>/', views.admin_truong_delete, name='admin_truong_delete'),
     
 ]
