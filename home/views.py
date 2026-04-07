912eee58d87ff5d0b2612cffa8d4b079a41997f3
Quockhaai16062005 <1250080078@sv.hcmunre.edu.vn>
2026-04-07 10:50:30 +0700

    Lưu các thay đổi

--- home/views.py
+++ home/views.py
@@ -4,3 +4,11 @@ from .models import TruongDaiHoc
 def home_page(request):
     truong_noi_bat = TruongDaiHoc.objects.all().order_by("matruong")[:3]
     return render(request, "home/home.html", {"truong_noi_bat": truong_noi_bat})
+def gioithieu(request):
+    # Lấy mã vai trò từ session (được lưu khi đăng nhập thành công)
+    mavaitro = request.session.get('mavaitro')
+    
+    # Kiểm tra nếu mã vai trò khớp với mã Admin trong database của bạn
+    is_admin = (mavaitro == 'VT001')
+    
+    return render(request, 'gioithieu/gioithieu.html', {'is_admin': is_admin})
